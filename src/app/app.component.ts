b95d1f6e7376dd4d07d16197916b7e3d4d4da2eb
Ludmi Lopez <ludmit@outlook.com>
2022-09-01 11:17:57 -0300

    getting-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WheatherAppAngular';
+export class AppComponent implements OnInit {
+
+  constructor(private weahterService: WeatherService){
+
+  }
+  ngOnInit(): void {
+    this.weahterService.getWeatherData('Buenos Aires')
+    .subscribe({
+      next:(response) => {
+        console.log(response)
+      }
+    });
+
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'db9519c257mshfd63b88785770fdp1acd98jsn5bc2c9ca9dde'
 };
 
 /*
