81dd41f9df88632c67d7800f8556535832be6ca1
Ludmi Lopez <ludmit@outlook.com>
2022-09-01 18:26:45 -0300

    added-firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'db9519c257mshfd63b88785770fdp1acd98jsn5bc2c9ca9dde'
 };
